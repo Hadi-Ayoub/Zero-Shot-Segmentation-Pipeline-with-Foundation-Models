2b4231d635876b5644e256dc54dd0d3bcb2d0ee7
Ayoub <ayoubbiof2@gmail.com>
2025-11-22 23:53:07 +0100

    batch processing and FP16 optimization

--- demo_pipeline.py
+++ demo_pipeline.py
@@ -9,80 +9,117 @@ from segment_anything import sam_model_registry, SamPredictor
 import torch 
 
 
-IMAGE_PATH = "/home/ayoub/DL/GroundingDINO/test_images/meeting.jpg"       
-TEXT_PROMPT = "microphone"          
+IMAGE_SRC = "test_images"
+TEXT_PROMPT = "dog"          
 BOX_THRESHOLD = 0.35         
 TEXT_THRESHOLD = 0.25
 
-DINO_CONFIG = "/home/ayoub/DL/GroundingDINO/groundingdino/config/GroundingDINO_SwinT_OGC.py"
-DINO_CHECKPOINT = "/home/ayoub/DL/GroundingDINO/weights/groundingdino_swint_ogc.pth"
-SAM_CHECKPOINT = "/home/ayoub/DL/GroundingDINO/weights/sam_vit_b_01ec64.pth"
+DINO_CONFIG = "groundingdino/config/GroundingDINO_SwinT_OGC.py"
+DINO_CHECKPOINT = "weights/groundingdino_swint_ogc.pth"
+SAM_CHECKPOINT = "weights/sam_vit_b_01ec64.pth"
 SAM_TYPE = "vit_b"
 
 DEVICE = torch.device("cuda")
 
 def main():
-    if not os.path.exists(IMAGE_PATH):
-        print(f"{IMAGE_PATH} is not found ! ")
-    image_source, image = load_image(IMAGE_PATH)
-    dino_model = load_model(DINO_CONFIG, DINO_CHECKPOINT, device=DEVICE)
 
-    print(f"Searching for : '{TEXT_PROMPT}'...")
-    boxes, logits, phrases = predict(
-        model=dino_model,
-        image=image,
-        caption=TEXT_PROMPT,
-        box_threshold=BOX_THRESHOLD,
-        text_threshold=TEXT_THRESHOLD
-    )
-    print(f"{len(boxes)} objects detected")
-    
-    # VRAM optimisation 
+    results_file = "segmentation_output"
+    os.makedirs(results_file, exist_ok = True)
+    for f in os.listdir(results_file):
+        os.remove(os.path.join(results_file, f))
+
+    valid_ext = ('.jpg', '.jpeg', '.png')
+    list_img_src = [f for f in os.listdir(IMAGE_SRC) if f.lower().endswith(valid_ext)]
+
+    dino_model = load_model(DINO_CONFIG, DINO_CHECKPOINT, device=DEVICE)
+    print(f"Searching for '{TEXT_PROMPT}' in {len(list_img_src)} images...")
+    dino_results = {}
+
+    with torch.inference_mode():
+        for img_name in list_img_src:
+            image_source, image = load_image(os.path.join(IMAGE_SRC, img_name))
+            
+            # GPU optimization : Autocast for FP16
+            with torch.cuda.amp.autocast():
+                boxes, logits, phrases = predict(
+                    model=dino_model,
+                    image=image,
+                    caption=TEXT_PROMPT,
+                    box_threshold=BOX_THRESHOLD,
+                    text_threshold=TEXT_THRESHOLD
+                )
+            
+
+            if boxes.numel() > 0:
+                dino_results[img_name] = boxes
+            else:
+                print(f"Skipped {img_name} (No detection)")
+
+    # VRAM cleaning
     del dino_model
-    torch.cuda.empty_cache
+    torch.cuda.empty_cache()
+
+
+
+    ##### SAM #####
 
     print("Loading SAM ...")
     sam= sam_model_registry[SAM_TYPE](checkpoint=SAM_CHECKPOINT)
     sam.to(device=DEVICE)
-    predictor = SamPredictor(sam)
-    image_cv = cv2.imread(IMAGE_PATH)
-    image_cv = cv2.cvtColor(image_cv, cv2.COLOR_BGR2RGB)
-    predictor.set_image(image_cv)
+    sam_predictor = SamPredictor(sam)
 
-    H, W, _ = image_cv.shape
-    boxes_xyxy = box_cxcywh_to_xyxy(boxes) * torch.Tensor([W, H, W, H])
-
-    transformed_boxes = predictor.transform.apply_boxes_torch(
-        boxes_xyxy.to(DEVICE), image_cv.shape[:2]
-    )
-
-
-    print("Segmenting ...")
-    masks, _, _ = predictor.predict_torch(
-        point_coords=None,
-        point_labels=None,
-        boxes=transformed_boxes,
-        multimask_output=False,
-    )
-
-    # visualizing
-    boxes_np = boxes_xyxy.cpu().numpy()
-    masks_np = masks.squeeze(1).cpu().numpy()
-
-    # annotating
-    detections = sv.Detections(
-        xyxy=boxes_np,
-        mask=masks_np,
-        class_id=np.array([0] * len(boxes)) # Tout est classe 0 pour l'instant
-    )
 
     box_annotator = sv.BoxAnnotator()
     mask_annotator = sv.MaskAnnotator()
 
-    annotated_image = mask_annotator.annotate(scene=image_cv.copy(), detections=detections)
-    annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections)
-    # saving image 
-    cv2.imwrite("rfinal_result.jpg", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+    print("Segmentation loop...")
+    
+
+    with torch.inference_mode():
+        for img_name, boxes in dino_results.items():
+            
+            full_path = os.path.join(IMAGE_SRC, img_name)
+            image_cv = cv2.imread(full_path)
+            image_cv = cv2.cvtColor(image_cv, cv2.COLOR_BGR2RGB)
+
+            
+            sam_predictor.set_image(image_cv)
+            H, W, _ = image_cv.shape
+
+            boxes = boxes.to(DEVICE)
+
+            scale_tensor = torch.tensor([W, H, W, H], device=DEVICE)
+            boxes_xyxy = box_cxcywh_to_xyxy(boxes) * scale_tensor
+            transformed_boxes = sam_predictor.transform.apply_boxes_torch(
+                boxes_xyxy, image_cv.shape[:2]
+            )
+
+            with torch.cuda.amp.autocast():
+                masks, _, _ = sam_predictor.predict_torch(
+                    point_coords=None,
+                    point_labels=None,
+                    boxes=transformed_boxes,
+                    multimask_output=False,
+                )
+
+            # Visualization
+            boxes_np = boxes_xyxy.cpu().numpy()
+            masks_np = masks.squeeze(1).cpu().numpy()
+
+            detections = sv.Detections(
+                xyxy=boxes_np,
+                mask=masks_np,
+                class_id=np.array([0] * len(boxes))
+            )
+
+            # Annotation
+            annotated_image = mask_annotator.annotate(scene=image_cv.copy(), detections=detections)
+            annotated_image = box_annotator.annotate(scene=annotated_image, detections=detections)
+            
+            # Save
+            output_path = os.path.join(results_file, f"res_{img_name}")
+            cv2.imwrite(output_path, cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+            print(f"Processed {img_name}")
 
 
 if __name__ == "__main__":
